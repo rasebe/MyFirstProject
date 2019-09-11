e90ffcef68ef171e9fae5d00da2d17636f6d05cb
Raluca <raluca@DESKFASTMK02.localdomain>
2019-09-11 20:54:41 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        //Data type
+        String breed = "Bulldog englez";
+        String color = "maro";
+        float age = 6.5f;
+        byte weight = 30;
+        char gender = 'm';
+        int levelOfIntelligence = 377777877;
+        long levelOfEnergy = 377839999388888889L;
+        double needOfAttentionLevel = 24324342342342342d;
+
+
+        System.out.println(gender);
+        System.out.println("About my dog:");
+
+
+
+
+    }
+}
